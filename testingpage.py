ed841851f486b1633df28f54cb280369d5f60f79
Savannah <60404236+sxfigu19@users.noreply.github.com>
2020-03-11 14:33:22 -0400

    Update testingpage.py

--- testingpage.py
+++ testingpage.py
@@ -2,18 +2,15 @@ import pyglet
 
 win = pyglet.window.Window(fullscreen=True)
 
-t = 10
+t = 90
 timerLabel = pyglet.text.Label(str(t), font_size=65,
                                        x=win.width // 15,
                                        y=win.height // 1.05,
                                        anchor_x='center', anchor_y='center')
 
-corner1= pyglet.image.load('images/assets/ground/tundra/slice01_01.png')
-
-grey = []
-for i in range(14):
-    for j in range(10):
-        grey.append(pyglet.sprite.Sprite(corner1, x = i * 48, y = j * 50))
+corner1= pyglet.image.load('images/assets/ground/tundra/slice19_19.png')
+c1 = pyglet.sprite.Sprite(corner1, x=0, y=0)
+c1.scale = 1
 
 def update(dt):
     global t
@@ -26,8 +23,7 @@ def update(dt):
 def on_draw():
     win.clear()
     timerLabel.draw()
-    for i in range(140):
-        grey[i].draw()
+    c1.draw()
 
 pyglet.clock.schedule(update)
 pyglet.app.run()
